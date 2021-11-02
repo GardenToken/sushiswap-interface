51e1d44286152155003088890a1ac37f12958e5d
BscClassic <ah0229247@gmail.com>
2021-11-02 19:18:38 +0700

    ccc

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,15 +57,15 @@ function AppBar(): JSX.Element {
                           {i18n._(t`Pool`)}
                         </a>
                       </NavLink>
-                      //{chainId && featureEnabled(Feature.MIGRATE, chainId) && (
+                      {chainId && featureEnabled(Feature.MIGRATE, chainId) && (
                         //<NavLink href={'/migrate'}>
-                          //<a
-                            //id={`migrate-nav-link`}
-                            //className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
-                          //>
-                            //{i18n._(t`Migrate`)}
-                          //</a>
-                        //</NavLink>
+                        //  <a
+                        //    id={`migrate-nav-link`}
+                        //    className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
+                        //  >
+                        //    //{i18n._(t`Migrate`)}
+                        //  </a>
+                       //</NavLink>
                       //)}
                       //{chainId && featureEnabled(Feature.LIQUIDITY_MINING, chainId) && (
                         <NavLink href={'/farm'}>
