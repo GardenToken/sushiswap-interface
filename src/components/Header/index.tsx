7230bd9e219a2954a3cd7f2c0ee7bf98e74c1217
BscClassic <ah0229247@gmail.com>
2021-11-02 20:11:01 +0700

    file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -63,7 +63,7 @@ function AppBar(): JSX.Element {
                             id={`migrate-nav-link`}
                             className="p-2 text-baseline text-primary hover:text-high-emphesis focus:text-high-emphesis md:p-3 whitespace-nowrap"
                           >
-                            //{i18n._(t`Migrate`)}
+                            {i18n._(t`Migrate`)}
                           </a>
                        </NavLink>
                       )}
